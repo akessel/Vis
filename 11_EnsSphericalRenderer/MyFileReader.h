26a3fc799acde302b0fba8f9e71ba755b9b862d9
akessel <andrew.kessel@colorado.edu>
2011-03-06 17:17:04 -0700

    fixed (?) file reader

--- 11_EnsSphericalRenderer/MyFileReader.h
+++ 11_EnsSphericalRenderer/MyFileReader.h
@@ -27,35 +27,14 @@ class MyFileReader
 		
 		T GetNextValue()
 		{
-			printf("Getting next value\n");
 			this->EnsureFileIsOpen();
 			
 			int size = sizeof(T);
 			char buffer [size];
 			
-			printf("Reading value\n");
 			this->stream.read(buffer, size);
-//			long newPointer = this->stream.tellg() + (long)size;
-//			this->stream.seekg(newPointer);
-
-			printf("Temping value\n");
-			char temp0 = buffer[0];
-			char temp1 = buffer[1];
-			char temp2 = buffer[2];
-			char temp3 = buffer[3];
-			printf("Reversing value\n");
-			buffer[0] = ReverseBits(temp3);
-			printf("Reversing done (1), next\n");
-			buffer[1] = ReverseBits(temp2);
-			printf("Reversing done (2), next\n");
-			buffer[2] = ReverseBits(temp1);
-			printf("Reversing done (3), next\n");
-			buffer[3] = ReverseBits(temp0);
-			printf("Reversing done (4), next\n");
 			
-			T result = (T)(*buffer);
-			
-			printf("result = %f\n", result);
+			T result = *((T*)buffer);
 			
 			return result;
 		}
