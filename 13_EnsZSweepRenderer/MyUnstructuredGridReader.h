abb9c1d97dbd5a0a5a37559ae1c1394498ed561c
akessel <andrew.kessel@colorado.edu>
2011-03-29 14:21:25 -0600

    made sure reader for 13 was up to date

--- 13_EnsZSweepRenderer/MyUnstructuredGridReader.h
+++ 13_EnsZSweepRenderer/MyUnstructuredGridReader.h
@@ -60,11 +60,6 @@ class MyUnstructuredGridReader
 			points->SetNumberOfPoints(numPoints);
 			scalars->SetNumberOfValues(numPoints);
 
-			float max = -1000000.0;
-			float min = 1000000.0;
-			
-			int minIdx = 100000000;
-			int maxIdx = -100000000;
 			/*
 			this->nLong = 2;
 			this->nLat = 2;
@@ -81,8 +76,8 @@ class MyUnstructuredGridReader
 						
 						float value = this->reader.GetNextValue();
 						
-						if(value > max) max = value;
-						if(value < min) min = value;
+						//if(value > max) max = value;
+						//if(value < min) min = value;
 						
 						double rad = k * ( (this->maxRad - this->minRad) / this->nRad ) + this->minRad;
 						double phi = j * ( (this->maxLat - this->minLat) / this->nLat ) + this->minLat;
@@ -92,100 +87,63 @@ class MyUnstructuredGridReader
 						double y = rad * sin(phi*PI/180.0);
 						double z = rad * cos(theta*PI/180.0) * cos(phi*PI/180.0);
 						/*
-						double x = (double)k;
-						double y = (double)j;
-						double z = (double)i;
+						x = (double)k;
+						y = (double)j;
+						z = (double)i;
 						*/
 						points->InsertPoint(count, x, y, z);									
 						scalars->InsertValue(count, value);
 				
 						count++;
+						
+						int FLT;	// index of Front	Left	Top
+						int FLB;	// index of Front	Left	Bottom
+						int FRT;	// index of Front	Right	Top
+						int FRB;	// index of Front	Right	Bottom
+						int BLT;	// index of Back	Left	Top
+						int BLB;	// index of Back	Left	Bottom
+						int BRT;	// index of Back	Right	Top
+						int BRB;	// index of Back	Right	Bottom
 
 						if(i > 0 && j > 0 && k > 0)
 						{
-							// find the indices of the points defining the vertices of the cube
-							int FLT = this->LinearizeCoordinate(i-1,j-1,k-1);	// index of Front	Left	Top
-							int FLB = this->LinearizeCoordinate(i-1,j,  k-1);	// index of Front	Left	Bottom
-							int FRT = this->LinearizeCoordinate(i-1,j-1,k);	// index of Front	Right	Top
-							int FRB = this->LinearizeCoordinate(i-1,j,  k);		// index of Front	Right	Bottom
-							int BLT = this->LinearizeCoordinate(i,	j-1,k-1);	// index of Back	Left	Top
-							int BLB = this->LinearizeCoordinate(i,	j,  k-1);		// index of Back	Left	Bottom
-							int BRT = this->LinearizeCoordinate(i,	j-1,k);		// index of Back	Right	Top
-							int BRB = this->LinearizeCoordinate(i,	j,  k);		// index of Back	Right	Bottom
+							FLT = this->LinearizeCoordinate(i-1,j-1,k-1);
+							FLB = this->LinearizeCoordinate(i-1,j,  k-1);
+							FRT = this->LinearizeCoordinate(i-1,j-1,k);
+							FRB = this->LinearizeCoordinate(i-1,j,  k);
+							BLT = this->LinearizeCoordinate(i,	j-1,k-1);
+							BLB = this->LinearizeCoordinate(i,	j,  k-1);
+							BRT = this->LinearizeCoordinate(i,	j-1,k);
+							BRB = this->LinearizeCoordinate(i,	j,  k);
 							
-							if(FLT < minIdx) minIdx = FLT;
-							if(FLB < minIdx) minIdx = FLB;
-							if(FRT < minIdx) minIdx = FRT;
-							if(FRB < minIdx) minIdx = FRB;
-							if(BLT < minIdx) minIdx = BLT;
-							if(BLB < minIdx) minIdx = BLB;
-							if(BRT < minIdx) minIdx = BRT;
-							if(BRB < minIdx) minIdx = BRB;
+							this->InsertTetrahedrons(FLT, FLB, FRT, FRB, BLT, BLB, BRT, BRB, cells);
+						}/*						
+						else if(i == 0 && j == 0)
+						{
+							// wrap around both longitude and latitude
+							FLT = this->LinearizeCoordinate(this->nLong-1,this->nLat-1,k-1);
+							FLB = this->LinearizeCoordinate(this->nLong-1,j,  k-1);
+							FRT = this->LinearizeCoordinate(this->nLong-1,this->nLat-1,k);
+							FRB = this->LinearizeCoordinate(this->nLong-1,j,  k);
+							BLT = this->LinearizeCoordinate(i,	this->nLat-1,k-1);
+							BLB = this->LinearizeCoordinate(i,	j,  k-1);
+							BRT = this->LinearizeCoordinate(i,	this->nLat-1,k);
+							BRB = this->LinearizeCoordinate(i,	j,  k);
+						}*/
+						else if(i == 0)
+						{
+							// wrap around longitude
+							FLT = this->LinearizeCoordinate(this->nLong-1,j-1,k-1);
+							FLB = this->LinearizeCoordinate(this->nLong-1,j,  k-1);
+							FRT = this->LinearizeCoordinate(this->nLong-1,j-1,k);
+							FRB = this->LinearizeCoordinate(this->nLong-1,j,  k);
+							BLT = this->LinearizeCoordinate(i,	j-1,k-1);
+							BLB = this->LinearizeCoordinate(i,	j,  k-1);
+							BRT = this->LinearizeCoordinate(i,	j-1,k);
+							BRB = this->LinearizeCoordinate(i,	j,  k);
 							
-							if(FLT > maxIdx) maxIdx = FLT;
-							if(FLB > maxIdx) maxIdx = FLB;
-							if(FRT > maxIdx) maxIdx = FRT;
-							if(FRB > maxIdx) maxIdx = FRB;
-							if(BLT > maxIdx) maxIdx = BLT;
-							if(BLB > maxIdx) maxIdx = BLB;
-							if(BRT > maxIdx) maxIdx = BRT;
-							if(BRB > maxIdx) maxIdx = BRB;
-							/*
-							printf("---------------\n");
-							printf("Cell [%i %i %i]\n", i, j, k);
-							printf("FLT = %i\n", FLT);
-							printf("FLB = %i\n", FLB);
-							printf("FRT = %i\n", FRT);
-							printf("FRB = %i\n", FRB);
-							printf("BLT = %i\n", BLT);
-							printf("BLB = %i\n", BLB);
-							printf("BRT = %i\n", BRT);
-							printf("BRB = %i\n", BRB);
-							*/
-							// each cube is composed of five tetrahedrons
-							vtkTetra *frontLeftCell = vtkTetra::New();
-							vtkTetra *frontRightCell = vtkTetra::New();
-							vtkTetra *backLeftCell = vtkTetra::New();
-							vtkTetra *backRightCell = vtkTetra::New();
-							vtkTetra *centerCell = vtkTetra::New();
-					
-							frontLeftCell->GetPointIds()->SetNumberOfIds(4);
-							frontRightCell->GetPointIds()->SetNumberOfIds(4);
-							backLeftCell->GetPointIds()->SetNumberOfIds(4);
-							backRightCell->GetPointIds()->SetNumberOfIds(4);
-							centerCell->GetPointIds()->SetNumberOfIds(4);
-							
-							frontLeftCell->GetPointIds()->SetId(0, FLB);
-							frontLeftCell->GetPointIds()->SetId(1, FRB);
-							frontLeftCell->GetPointIds()->SetId(2, BLB);
-							frontLeftCell->GetPointIds()->SetId(3, FLT);
-							
-							frontRightCell->GetPointIds()->SetId(0, FLT);
-							frontRightCell->GetPointIds()->SetId(1, BRT);
-							frontRightCell->GetPointIds()->SetId(2, FRT);
-							frontRightCell->GetPointIds()->SetId(3, FRB);
-							
-							backRightCell->GetPointIds()->SetId(0, FRB);
-							backRightCell->GetPointIds()->SetId(1, BRB);
-							backRightCell->GetPointIds()->SetId(2, BLB);
-							backRightCell->GetPointIds()->SetId(3, BRT);
-							
-							backLeftCell->GetPointIds()->SetId(0, FLT);
-							backLeftCell->GetPointIds()->SetId(1, BLT);
-							backLeftCell->GetPointIds()->SetId(2, BRT);
-							backLeftCell->GetPointIds()->SetId(3, BLB);
-							
-							centerCell->GetPointIds()->SetId(0, FLT);
-							centerCell->GetPointIds()->SetId(1, BRT);
-							centerCell->GetPointIds()->SetId(2, FRB);
-							centerCell->GetPointIds()->SetId(3, BLB);
-
-							cells->InsertNextCell(frontLeftCell);
-							cells->InsertNextCell(frontRightCell);
-							cells->InsertNextCell(backLeftCell);
-							cells->InsertNextCell(backRightCell);							
-							cells->InsertNextCell(centerCell);
-						}
+							this->InsertTetrahedrons(FLT, FLB, FRT, FRB, BLT, BLB, BRT, BRB, cells);
+						}						
 					}
 				}
 			}
@@ -200,8 +158,7 @@ class MyUnstructuredGridReader
 			printf("Ending get output...\n");
 			
 			printf("Count = %i\n", count);
-			printf("Min = %e, Max = %e\n", min, max);
-			printf("MinIdx = %i, MaxIdx = %i\n", minIdx, maxIdx);
+			//printf("Min = %e, Max = %e\n", min, max);
 			
 			return grid;		
 		}
@@ -214,6 +171,53 @@ class MyUnstructuredGridReader
 		int LinearizeCoordinate(int i, int j, int k)
 		{
 			return i*this->nLat*this->nRad + j*this->nRad + k;
+		}
+		
+		void InsertTetrahedrons(int FLT, int FLB, int FRT, int FRB, int BLT, int BLB, int BRT, int BRB, vtkCellArray *cells)
+		{
+			// each cube is composed of five tetrahedrons
+			vtkTetra *frontLeftCell = vtkTetra::New();
+			vtkTetra *frontRightCell = vtkTetra::New();
+			vtkTetra *backLeftCell = vtkTetra::New();
+			vtkTetra *backRightCell = vtkTetra::New();
+			vtkTetra *centerCell = vtkTetra::New();
+	
+			frontLeftCell->GetPointIds()->SetNumberOfIds(4);
+			frontRightCell->GetPointIds()->SetNumberOfIds(4);
+			backLeftCell->GetPointIds()->SetNumberOfIds(4);
+			backRightCell->GetPointIds()->SetNumberOfIds(4);
+			centerCell->GetPointIds()->SetNumberOfIds(4);
+			
+			frontLeftCell->GetPointIds()->SetId(0, FLB);
+			frontLeftCell->GetPointIds()->SetId(1, FRB);
+			frontLeftCell->GetPointIds()->SetId(2, BLB);
+			frontLeftCell->GetPointIds()->SetId(3, FLT);
+			
+			frontRightCell->GetPointIds()->SetId(0, FLT);
+			frontRightCell->GetPointIds()->SetId(1, BRT);
+			frontRightCell->GetPointIds()->SetId(2, FRT);
+			frontRightCell->GetPointIds()->SetId(3, FRB);
+			
+			backRightCell->GetPointIds()->SetId(0, FRB);
+			backRightCell->GetPointIds()->SetId(1, BRB);
+			backRightCell->GetPointIds()->SetId(2, BLB);
+			backRightCell->GetPointIds()->SetId(3, BRT);
+			
+			backLeftCell->GetPointIds()->SetId(0, FLT);
+			backLeftCell->GetPointIds()->SetId(1, BLT);
+			backLeftCell->GetPointIds()->SetId(2, BRT);
+			backLeftCell->GetPointIds()->SetId(3, BLB);
+			
+			centerCell->GetPointIds()->SetId(0, FLT);
+			centerCell->GetPointIds()->SetId(1, BRT);
+			centerCell->GetPointIds()->SetId(2, FRB);
+			centerCell->GetPointIds()->SetId(3, BLB);
+
+			cells->InsertNextCell(frontLeftCell);
+			cells->InsertNextCell(frontRightCell);
+			cells->InsertNextCell(backLeftCell);
+			cells->InsertNextCell(backRightCell);							
+			cells->InsertNextCell(centerCell);
 		}
 };
 
